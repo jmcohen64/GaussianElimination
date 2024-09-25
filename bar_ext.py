2b1fcb91dc19816644df1553356d62a42f39c4cf
Marek Rychlik <rychlik@arizona.edu>
2024-09-25 10:24:33 -0700

    *** empty log message ***

--- bar_ext.py
+++ bar_ext.py
@@ -8,5 +8,5 @@ lib.bar_add.argtypes = (ctypes.c_int, ctypes.c_int)
 lib.bar_add.restype = ctypes.c_int
 
 # Call the function
-result = lib.foo_add(5, 7)
+result = lib.bar_add(5, 7)
 print(result)  # Outputs: 12
