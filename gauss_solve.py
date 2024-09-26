0ddcf876b39360b4537cf23a5a1b5eef68a32f59
Marek Rychlik <rychlik@arizona.edu>
2024-09-26 09:34:24 -0700

    *** empty log message ***

--- gauss_solve.py
+++ gauss_solve.py
@@ -4,17 +4,16 @@ import ctypes
 lib = ctypes.CDLL('./libgauss.so')
 
 
-def lu(A):
+def lu_in_place(A):
     # Create a 2D array in Python and flatten it
-    rows, cols = 3, 3
-    array_2d = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-    flat_array_2d = [item for row in array_2d for item in row]
+    n = len(A)
+    flat_array_2d = [item for row in A for item in row]
 
     # Convert to a ctypes array
     c_array_2d = (ctypes.c_int * len(flat_array_2d))(*flat_array_2d)
 
     # Define the function signature
-    lib.modify_2d_array.argtypes = (ctypes.POINTER(ctypes.c_int), ctypes.c_int, ctypes.c_int, ctypes.c_int)
+    lib.lu_in_place.argtypes = (ctypes.POINTER(ctypes.c_int), ctypes.c_int, ctypes.c_int, ctypes.c_int)
 
     # Modify the array in C (e.g., add 10 to each element)
     lib.modify_2d_array(c_array_2d, rows, cols, 10)
