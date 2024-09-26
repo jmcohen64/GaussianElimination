758031d2693314519f9c3b83ece9d5d1550f6f82
Marek Rychlik <rychlik@arizona.edu>
2024-09-26 10:48:33 -0700

    *** empty log message ***

--- gauss_solve.py
+++ gauss_solve.py
@@ -38,7 +38,7 @@ def lu(A):
         for i in range(n)
     ]
 
-    # Extract L and U parts from A
+    # Extract L and U parts from A, add 0's and 1's
     L = [
         [[modified_array_2d[i][j] for j in range(i)] + [1] + [0 for j in range(i+1,n)]]
         for i in range(n)
