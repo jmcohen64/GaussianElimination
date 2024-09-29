59fe5e32e13d27d4032b466899c82383e6d2fcc8
Marek Rychlik <rychlik@arizona.edu>
2024-09-29 06:20:26 -0700

    *** empty log message ***

--- gauss_solve.h
+++ gauss_solve.h
@@ -11,6 +11,15 @@
 #ifndef GAUSS_SOLVE_H
 #define GAUSS_SOLVE_H
 
+
+/* An idiomatic way to swap two l-values X and Y of type TYPE in C */
+#define SWAP(X, Y, TYPE) do {			\
+    TYPE tmp = (X);				\
+    (X) = (Y);					\
+    (Y) = tmp;					\
+  } while(0)
+
+
 void gauss_solve_in_place(const int n, double A[n][n], double b[n]);
 void lu_in_place(const int n, double A[n][n]);
 void lu_in_place_reconstruct(int n, double A[n][n]);
